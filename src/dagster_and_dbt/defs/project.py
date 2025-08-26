df26f23d4c6382b9c3bb59e7eb4859740a9672a3
samruddhi naik <samruddhi.naik@mayden.co.uk>
2025-08-26 15:11:30 +0100

    updates to the project.py

--- src/dagster_and_dbt/defs/project.py
+++ src/dagster_and_dbt/defs/project.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from dagster_dbt import DbtProject
 
 dbt_project = DbtProject(
-  project_dir=Path(__file__).joinpath("../..", "analytics").resolve(),
+  project_dir=Path(__file__).joinpath("..", "analytics").resolve(),
 )
 
 dbt_project.prepare_if_dev()
